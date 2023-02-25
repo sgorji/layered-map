bfac4cafee478ba90c620eeda0b8db4958da61f7
sgorji <saeed.gorji@proton.me>
2023-02-25 22:02:26 +0100

    Limit the extra polygons updates to add button

--- src/App.js
+++ src/App.js
@@ -6,7 +6,8 @@ import Map from "./components/Map";
 function App() {
   const [showPolygon, setShowPolygon] = useState(false);
   const [readInput, setReadInput] = useState(0);
-  const [numPolygons, setNumPolygons] = useState(0);
+  // const [numPolygons, setNumPolygons] = useState(0);
+  const [extraPolygons, setExtraPolygons] = useState([]);
 
   function handleCheck() {
     setShowPolygon(!showPolygon);
@@ -15,8 +16,30 @@ function App() {
   function getInput(e) {
     setReadInput(parseInt(e.target.value));
   }
+
+  function generatePolygons(polygonCount) {
+    // Calculating the size of a square grid to fit extra polygons
+    const squreSide = Math.ceil(Math.sqrt(polygonCount));
+
+    const newPolygons = [];
+    for (let i = 0; i < polygonCount; i++) {
+      const randomCoordinates = [];
+
+      // Generate random points for the next polygon with grid shifts
+      for (let j = 0; j < 5; j++) {
+        const latitude =
+          59.0 + (Math.floor(i / squreSide) + Math.random()) / squreSide;
+        const longitude = 11.0 + ((i % squreSide) + Math.random()) / squreSide;
+        randomCoordinates.push([latitude, longitude]);
+      }
+      newPolygons.push(randomCoordinates);
+    }
+    return newPolygons;
+  }
+
   function handleAdd() {
-    setNumPolygons(readInput);
+    // setNumPolygons(readInput);
+    setExtraPolygons(generatePolygons(readInput));
   }
 
   return (
@@ -47,7 +70,7 @@ function App() {
           <input type="number" value={readInput} onChange={getInput} />
           <button onClick={handleAdd}>Add Polygon</button>
         </div>
-        <Map polygonFlag={showPolygon} extraPolygons={numPolygons}></Map>
+        <Map polygonFlag={showPolygon} extraPolygons={extraPolygons}></Map>
       </div>
     </div>
   );
--- src/components/Map.js
+++ src/components/Map.js
@@ -6,26 +6,6 @@ function Map({ polygonFlag, extraPolygons }) {
   const initialPosition = [59.9139, 10.7455]; // [latitude, longitude]
   const initialZoom = 10;
 
-  function generatePolygons(extraPolygons) {
-    // Calculating the size of a square grid to fit extra polygons
-    const squreSide = Math.ceil(Math.sqrt(extraPolygons));
-
-    const newPolygons = [];
-    for (let i = 0; i < extraPolygons; i++) {
-      const randomCoordinates = [];
-
-      // Generate random points for the next polygon with grid shifts
-      for (let j = 0; j < 5; j++) {
-        const latitude =
-          59.0 + (Math.floor(i / squreSide) + Math.random()) / squreSide;
-        const longitude = 11.0 + ((i % squreSide) + Math.random()) / squreSide;
-        randomCoordinates.push([latitude, longitude]);
-      }
-      newPolygons.push(randomCoordinates);
-    }
-    return newPolygons;
-  }
-
   // Coordinatas for the specified polygon
   const coordinates = [
     // [11.021941683607787, 59.944518214479572],
@@ -53,7 +33,7 @@ function Map({ polygonFlag, extraPolygons }) {
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
       {polygonFlag && <Polygon positions={coordinates} />}
-      {<Polygon positions={generatePolygons(extraPolygons)} />}
+      {<Polygon positions={extraPolygons} />}
     </MapContainer>
   );
 }
